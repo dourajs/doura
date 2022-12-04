f496ba4a33535aa92619696841366e1ef197eb94
jinyangwang <214997474@qq.com>
2022-12-04 17:59:45 +0800

    feat: support hmr

--- packages/react-doura/src/useModel.tsx
+++ packages/react-doura/src/useModel.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import { useMemo, useRef } from 'react'
 import { doura } from 'doura'
 import type { AnyModel, Selector } from 'doura'
 import { createBatchManager } from './batchManager'
@@ -17,11 +17,19 @@ const useModel: UseModel = <
     return [doura(), createBatchManager()]
   }, [])
 
+  const context = useRef({
+    douraStore,
+    batchManager,
+  })
+
   return useMemo(
     function () {
-      return createUseModel(douraStore, batchManager)
+      return createUseModel(
+        context.current.douraStore,
+        context.current.batchManager
+      )
     },
-    [douraStore, batchManager]
+    [context.current.douraStore, context.current.batchManager]
   )(model, selector, depends)
 }
 
