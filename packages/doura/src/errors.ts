b8efdf713674ec1f7854b417cfe965efdeea7978
liximomo <liximomo@gmail.com>
2023-02-23 18:29:02 +0800

    fix: log and throw errors

--- packages/doura/src/errors.ts
+++ packages/doura/src/errors.ts
@@ -1,3 +1,5 @@
+import { warn } from './warning'
+
 export const enum ErrorCodes {
   SCHEDULER,
 }
@@ -8,12 +10,20 @@ const Errors: Record<number | string, string> = {
     'Please open an issue at https://github.com/dourajs/doura/issues/new',
 }
 
-export function error(_err: unknown, type: ErrorCodes, ...args: any[]) {
-  const e = Errors[type] as any
-  const msg = !e
-    ? 'unknown error nr: ' + error
-    : typeof e === 'function'
-    ? e.apply(null, args as any)
-    : e
-  throw new Error(`[Doura] ${msg}`)
+export function error(err: unknown, type: ErrorCodes, ...args: any[]) {
+  if (__DEV__) {
+    const e = Errors[type] as any
+    const info = !e
+      ? 'unknown error nr: ' + error
+      : typeof e === 'function'
+      ? e.apply(null, args as any)
+      : e
+    warn(`Unhandled error${info ? ` during execution of ${info}` : ``}`)
+
+    // crash in dev by default so it's more noticeable
+    throw err
+  } else {
+    // recover in prod to reduce the impact on end-user
+    console.error(err)
+  }
 }
